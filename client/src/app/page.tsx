7edbf68941635749cf0fe46fbf1081be1815d0c1
Momen Yasser <momenyasser522@gmail.com>
2025-10-09 19:23:50 +0300

    Add Radix UI slot dependency and update page layout
    - Added @radix-ui/react-slot dependency to package.json for improved component composition.
    - Refactored the main page layout to enhance user experience with a new design featuring a welcome message and login form.
    - Removed outdated content and streamlined the structure for better readability and functionality.

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,103 +1,36 @@
-import Image from "next/image";
+'use client'
+
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="w-screen h-screen flex flex-row">
+      <div className="w-1/2 flex flex-col justify-center bg-neutral-5 gap-8">
+        <p className="text-3xl font-semibold tracking-tight w-fit pl-6">
+          Welcome to <span className="text-[#00D100]">Fresh Mart,</span> we deliver groceries to
+          your door step{' '}
+        </p>
+        <p className="text-xl tracking-tight w-fit pl-6">
+          Save time, skip the lines, and enjoy the convenience of quick, efficient delivery
+        </p>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+      <div className="w-1/2 bg-[#00D100] h-screen flex justify-center items-center">
+        <div className="bg-white rounded-4xl min-w-[50%] min-h-[500px] flex flex-col justify-center items-center shadow-xl gap-20">
+          <p className="font-semibold text-3xl">Login</p>
+          <div className="flex flex-col justify-center items-center w-full gap-4">
+            <input type="text" placeholder="Email" className="w-[75%] h-12 rounded-4xl border-2 pl-6" />
+            <input
+              type="password"
+              placeholder="Password"
+              className="w-[75%] h-12 rounded-4xl border-2 pl-6"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+          <Button onClick={() => {}} className="bg-[#00D100] w-[75%] h-11 rounded-4xl hover:bg-[#00A300]">
+            LOGIN
+          </Button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
-  );
+  )
 }
